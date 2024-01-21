cfb7b27cfa77781737192fe01381825a7a29f8c4
Anas Zenagui <zenaguianas20@gmail.com>
2024-01-22 00:17:58 +0100

    Initializing the airflow + making the recommendations of insurances based on CF

---
+++ dags/our_first_dag.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+# pip install apache-airflow-providers-apache-livy
+
+default_args = {
+  'owner': 'coder2j',
+  'retries': 5,
+  'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+  dag_id='our_first_dag_v2',
+  default_args=default_args,
+  description="This is our first dag we create",
+  start_date=datetime(2024, 1, 17, 5),
+  schedule_interval='@daily'
+) as dag:
+  task1 = BashOperator(
+    task_id='first_task',
+    bash_command="echo hello world, Anas, This the first task 1!" 
+  )
+
+  task2 = BashOperator(
+    task_id='second_task',
+    bash_command="echo hello world, Anas, This the second task 2!" 
+  )
+
+  task3 = BashOperator(
+    task_id='third_task',
+    bash_command="echo hello world, Anas, This the third task 3!" 
+  )
+
+  task1 >> [task2, task3]
